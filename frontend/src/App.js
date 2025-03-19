5b325e709b3a29288a99434ef6b54535cd2ed504
Jose Vargas <josepavaa@yahoo.es>
2025-03-18 23:51:02 -0600

    Working about section. Fixed order on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,8 @@ const App = () => {
   return (
     <div className="app">
       <NavBar />
-      <About />
       <Header />
+      <About />
       <Work />
       <Skills />
       <Testimonial />
--- frontend/src/container/about/About.jsx
+++ frontend/src/container/about/About.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './About'
+import { useState, useEffect } from 'react'
+import {motion} from 'framer-motion'
+import './About.scss'
+import { images } from '../../constants'
+const abouts= [
+  {title: 'Quality Assurance' , description: 'Senior Automation Engineer', imgUrl:images.about01},
+  {title: 'Test Automation' , description: 'Senior Automation Engineer', imgUrl:images.about02},
+  {title: 'UI/UX Testing' , description: 'Senior Automation Engineer', imgUrl:images.about03},
+  {title: 'Backend Testing' , description: 'Senior Automation Engineer', imgUrl:images.about04}
+]
 
-const about = () => {
+const About = () => {
   return (
-    <div>about</div>
+    <> 
+      <h2 className="head-text">I Know that <span>Quality</span> <br />means  <span>Customer Trust</span></h2>
+      <div className="app__profiles">
+      {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
-export default about
+export default About
